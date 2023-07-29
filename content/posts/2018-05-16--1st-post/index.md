258971a3e7e82e03da87f2fcf31d2fb27ff35de6
gyneco2d <mikeda2501@gmail.com>
2023-07-29 17:59:55 +0900

    Upgrade deps & change gatsby theme

--- content/posts/2018-05-16--1st-post/index.md
+++
@@ -1,9 +0,0 @@
----
-title: 最初
-subTitle: はじめました
-category: "blog"
-cover: ../article_icons/avatar.jpg
----
-
-## 最初の投稿
-かくよ
---
+++ content/posts/2018-05-24--customize-terminal/Gif.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import prompt from "./kawaii_prompt.gif"
+
+const Gif = () => {
+  return (
+    <img src={prompt} />
+  )
+}
+
+export default Gif
